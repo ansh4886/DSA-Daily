ff0fea82634bfcb8e31493243cf94f1952a396ce
Ansh <anshchouksey08@gmail.com>
2026-04-18 11:15:09 +0530

    Create Flip to Maximize 1s.cpp

---
+++ Flip to Maximize 1s.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int maxOnes(vector<int>&v){
+        int n = v.size();
+        int one = 0;
+        for(int i=0;i<n;i++){
+            if(v[i]==1) one++;
+        }
+        
+        int maxgain = 0;
+        int c = 0;
+        
+        for(int i=0;i<n;i++){
+            if(v[i]==0) c++;
+            else c--;
+            
+            if(c < 0) c = 0;
+            maxgain = max(maxgain,c);
+        }
+        return one+maxgain;
+    }
+};
+//GFG POTD solution for 18 April
