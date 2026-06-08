e9e78bef378c78f748b8fc3952ab0db17918ce04
Ansh <anshchouksey08@gmail.com>
2026-06-08 18:39:00 +0530

    Create Delete Nodes with Greater on Right.cpp

---
+++ Delete Nodes with Greater on Right.cpp
@@ -0,0 +1,47 @@
+/* Structure of a Linked List node
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    Node *compute(Node *head) {
+        // code here
+          deque<Node*> dq;
+    Node* curr = head->next;
+    dq.push_back(head);
+    
+    // Traversing LL
+    while(curr) {
+        // Condition
+        while(!dq.empty() && dq.back()->data < curr->data) {
+            dq.pop_back();
+        }
+        
+        dq.push_back(curr);
+        curr = curr->next;
+    }
+    
+    // Maintaining head, Last element of LL is always in the queue
+    head = dq.front();
+    dq.pop_front();
+    curr = head;
+    
+    // Stitching LL
+    while(!dq.empty()) {
+        curr->next = dq.front();
+        dq.pop_front();
+        curr = curr->next;
+    }
+    
+    return head;
+    }
+};
+//GFG POTD solution for 08 June
